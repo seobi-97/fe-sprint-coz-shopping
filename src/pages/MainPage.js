93c2bd756d29867ad3e5bf9e5f9b4208e1d16be2
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2023-05-17 14:06:44 +0900

    fetch를 이용한 api통신

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,17 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Dropdown from "../components/Dropdown";
 import Footer from "../components/Footer";
+import { useSelector, useDispatch } from "react-redux";
+import { setProduct } from "../redux/product";
+import Card from "../components/Card";
 
 function MainPage() {
-  const [isopen, setOpen] = useState(false);
+  const [isOpen, setOpen] = useState(false);
+  const product = useSelector((state) => state.product.value);
+  const dispatch = useDispatch();
+
   const handleClick = () => {
-    setOpen(!isopen);
+    setOpen(!isOpen);
   };
+
+  useEffect(() => {
+    fetch("http://cozshopping.codestates-seb.link/api/v1/products")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        dispatch(setProduct(res));
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <>
       <Header handleClick={() => handleClick()} />
-      {isopen ? <Dropdown /> : <></>}
+      {isOpen ? <Dropdown /> : <></>}
+      <Card />
       <Footer />
     </>
   );
