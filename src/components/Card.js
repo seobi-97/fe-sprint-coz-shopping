7ce18a63dc75cf44376812942994c077f9f7e792
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2023-05-18 23:55:08 +0900

    product list 보여주기 및 localStorage저장

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,48 +1,83 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
 import { styled } from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-`;
-const ContentBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  img {
-    width: 260px;
-    height: 210px;
-    margin: 15px;
-  }
-  p {
-    font-size: 10px;
-  }
-`;
+import bookmark from "../images/bookmark2.svg";
+import bookmarkOn from "../images/bookmarkOn.svg";
 
 const Box = styled.div`
   width: 290px;
   height: 260px;
+  margin: 10px;
+  p {
+    font-weight: 700;
+    font-size: 12px;
+  }
 `;
-function Card() {
-  const product = useSelector((state) => state.product.value);
-  const arr = product && product.slice(0, 4);
+const Bookmark = styled.img`
+  position: relative;
+  top: -10px;
+  right: 30px;
+  z-index: 1;
+  width: 20px;
+  height: 20px;
+`;
+const Picture = styled.img`
+  width: 260px;
+  height: 210px;
+  box-sizing: border-box;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 12px;
+  cursor: pointer;
+`;
+
+function Card({ product, modalOpen, productUpdate }) {
+  const [isOn, setIsOn] = useState(false);
+  const localStorage = window.localStorage;
+  const bookmarkStorage = JSON.parse(localStorage.getItem("bookmark"));
+
+  const handleClick = (id) => {
+    setIsOn(!isOn);
+    //bookmark라는 localStorage가 생성되지 않았거나, 빈 배열일때
+    if (bookmarkStorage === null || bookmarkStorage.length === 0) {
+      localStorage.setItem("bookmark", JSON.stringify([id]));
+    } else {
+      //bookmarkStorage에 이미 존재할 때
+      if (bookmarkStorage.filter((val) => val === id).length !== 0) {
+        localStorage.setItem(
+          "bookmark",
+          JSON.stringify(bookmarkStorage.filter((val) => val !== id))
+        );
+      } else {
+        //bookmarkStorage에 존재하지 않을 때 추가해준다.
+        localStorage.setItem(
+          "bookmark",
+          JSON.stringify([...bookmarkStorage, id])
+        );
+      }
+    }
+  };
   return (
-    <Container>
-      <ContentBox>
-        {arr ? (
-          arr.map((val) => (
-            <Box key={val.id}>
-              <img src={val.image_url || val.brand_image_url} />
-              <p>{val.title || val.brand_name}</p>
-            </Box>
-          ))
-        ) : (
-          <div></div>
-        )}
-      </ContentBox>
-    </Container>
+    <>
+      {product ? (
+        product.map((val) => (
+          <Box key={val.id}>
+            <Picture
+              onClick={() => {
+                modalOpen();
+                productUpdate(val);
+              }}
+              src={val.image_url || val.brand_image_url}
+            />
+            <Bookmark
+              onClick={() => handleClick(val.id)}
+              src={isOn ? bookmarkOn : bookmark}
+            />
+            <p>{val.title || val.brand_name}</p>
+          </Box>
+        ))
+      ) : (
+        <div></div>
+      )}
+    </>
   );
 }
 
